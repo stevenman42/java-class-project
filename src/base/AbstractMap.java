6c1142a2b2f7684350f38ee528bcf826a6952b02
wwolfe <ChadWolfie@gmail.com>
2015-04-20 19:49:33 -0400

    meep
    meep

--- src/base/AbstractMap.java
+++ src/base/AbstractMap.java
@@ -1,9 +1,50 @@
 package base;
 
+import java.awt.Graphics;
+
 public abstract class AbstractMap {
 	
+	/**
+	 * the handler for the map
+	 */
+	private Handler handler;
+	
+	/**
+	 * Default constructor
+	 */
 	public AbstractMap(){
-		
+		handler = new Handler();
+	}
+	
+	/**
+	 * runs the tick() method for all GameObjects in the map
+	 */
+	public void tick(){
+		handler.tick();
+	}
+	
+	/**
+	 * runs the render() method for all the objects in the game
+	 * @param g
+	 */
+	public void render(Graphics g){
+		handler.render(g);
+	}
+	
+	/**
+	 * adds a GameObject to the map
+	 * @param go
+	 */
+	public void addGameObject(GameObject go){
+		handler.addObject(go);
+	}
+	
+	/**
+	 * removes the GameObject to the map
+	 * @param go
+	 */
+	public void removeGameObject(GameObject go){
+		handler.removeObject(go);
 	}
 	
 }
