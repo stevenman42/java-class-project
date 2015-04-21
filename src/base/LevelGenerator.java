2257d4aff13d9a6e74618487c80e863178250bde
Steven <stevenman42@gmail.com>
2015-04-21 19:21:50 -0400

    I have no clue what I did to all those classes

--- src/base/LevelGenerator.java
+++ src/base/LevelGenerator.java
@@ -1,8 +1,14 @@
 package base;
 import java.util.ArrayList;
+import java.lang.Math;
+
 
 class Room{
-	// this is a stub
+	// this is a stub, as you can see
+	// I just made this to shut up the stupid IDE
+	// I expect that we're going to have an actual class that represents the rooms at some point, and that will replace this one
+	// Not in this file though probably, and it might not be called Room
+	// System.out.println("YOLOOOO");
 }
 
 /**
@@ -14,11 +20,34 @@ class Room{
  */
 public class LevelGenerator {
 	
+	private int floorID = 0;
+	private int wallID = 1;
+	private int doorID = 2;
+	
 	public int[][] CreateLevel(ArrayList<Room> rooms){
 		int[][] newLevel = new int[128][128];
 		
 		return newLevel;
 	};
+	
+	
+	/**
+	 * 
+	 * This class only needs to be used when creating a "default" room with no parameters
+	 * 
+	 * @return newRoom
+	 */
+	private int[][] GenerateRoom(){
+		int[][] newRoom = new int[(int)Math.random() * 7 + 10][(int)Math.random() * 7 + 10];
+		for (int i = 0; i < newRoom[0].length; i ++){
+			newRoom[0][i] = wallID;
+		}
+		
+		
+		
+		
+		return newRoom;
+	}
 	
 	
 	/**
@@ -30,6 +59,11 @@ public class LevelGenerator {
 	 */
 	public int[][] CreateLevel(){
 		int[][] newLevel = new int[128][128];
+		
+		
+		
+		
+		
 		
 		return newLevel;
 	}
