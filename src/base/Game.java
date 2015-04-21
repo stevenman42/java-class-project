75ad169c33774eef63e2a823d28ca4bd899a3b8a
Steven <stevenman42@gmail.com>
2015-04-21 19:29:31 -0400

    thanks chad

--- src/base/Game.java
+++ src/base/Game.java
@@ -19,6 +19,7 @@ public class Game extends Canvas implements Runnable{
 
 	//private static final long serialVersionUID = 3054892105735692509L;
 	// thanks chad
+	// private static final long serialVersionID = chad's_mom's_phone_number;
 	private static final long serialVersionUID = 6969696969696969696L;
 	
 	//set window size
