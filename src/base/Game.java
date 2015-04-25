bfe3e674992c6e77bf0968be951c6b62c4be5356
bespers <bespeboy@gmail.com>
2015-04-24 23:15:38 -0400

    f
    f

--- src/base/Game.java
+++ src/base/Game.java
@@ -212,7 +212,7 @@ public class Game extends Canvas implements Runnable{
 			g.setColor(Color.red);
 		}
 		else{
-			g.setColor(new Color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256)));
+			g.setColor(Color.blue);
 		}
 		
 		g.fillRect(0, 0, WIDTH, HEIGHT);
