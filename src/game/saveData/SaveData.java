0df282bd5fd2ce19529ed0f354fceafd635df1f1
wwolfe <ChadWolfie@gmail.com>
2015-04-30 16:30:08 -0400

    herpies

--- src/game/saveData/SaveData.java
+++ src/game/saveData/SaveData.java
@@ -2,18 +2,38 @@ package game.saveData;
 
 public class SaveData {
 	
+	/**
+	 * the key that was read
+	 */
 	private String key;
+	
+	/**
+	 * the data that was read
+	 */
 	private String data;
 	
+	/**
+	 * constructor
+	 * @param key	the key
+	 * @param data	the data
+	 */
 	public SaveData(String key, String data){
 		this.key = key;
 		this.data = data;
 	}
 	
+	/**
+	 * returns the key
+	 * @return
+	 */
 	public String getKey(){
 		return key;
 	}
 	
+	/**
+	 * returns the data
+	 * @return
+	 */
 	public String getData(){
 		return data;
 	}
---
+++ src/game/saveData/SaveDataHandler.java
@@ -0,0 +1,17 @@
+package game.saveData;
+
+import java.util.ArrayList;
+
+public class SaveDataHandler {
+	
+	private ArrayList<SaveData> data;
+	
+	public SaveDataHandler(){
+		data = new ArrayList<SaveData>();
+	}
+	
+	public void readSaveData(){
+		
+	}
+	
+}
