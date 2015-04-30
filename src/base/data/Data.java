3db489d9862c0d8982a250692fc0cb8d7d2b2a04
wwolfe <ChadWolfie@gmail.com>
2015-04-30 15:53:47 -0400

    adsf

--- src/base/data/Data.java
+++ src/base/data/Data.java
@@ -1,14 +1,5 @@
 package base.data;
 
-public class Data{
-	
-	//private SaveableData sd;
-	
-	//private String key;
-	
-	public Data(String key, SaveableData s){
-		//this.key = key;
-		//sd = s;
-	}
-	
+public class Data {
+
 }
--- src/base/data/SaveableData.java
+++
@@ -1,9 +0,0 @@
-package base.data;
-
-public interface SaveableData <E>{
-	
-	public String getData();
-	
-	public E readData();
-	
-}
