7ff14664173e39fc0c04c04b3e19102207e6f4dd
Steven <stevenman42@gmail.com>
2015-04-30 06:26:12 -0400

    why am I committing this I did almost nothing

--- src/base/AI.java
+++ src/base/AI.java
@@ -7,6 +7,7 @@ import base.Entities.Player;
 public class AI {
 	
 	//Steven this is where all your static AI pathfinding stuff will go
+	// Yay ty bb
 	
 	public static Point findPlayer(){
 		return new Point((int)Player.staticX, (int)Player.staticY);
