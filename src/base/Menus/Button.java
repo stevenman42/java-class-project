e850f4aa3fb68450e7651a5a6dbbbca7c9622f32
wwolfe <ChadWolfie@gmail.com>
2015-05-05 20:48:43 -0400

    god has saved me

--- src/base/Menus/Button.java
+++ src/base/Menus/Button.java
@@ -65,12 +65,10 @@ public class Button extends AbstractGuiComponent{
 	}
 	
 	protected void renderBackground(Graphics2D g2d){
-		if(backgroundColor != null){
-			Color prev = g2d.getColor();
-			g2d.setColor(backgroundColor);
-			g2d.draw(rect);
-			g2d.setColor(prev);
-		}
+		Color prev = g2d.getColor();
+		g2d.setColor(backgroundColor);
+		g2d.fill(rect);
+		g2d.setColor(prev);
 	}
 	
 	protected void renderBorder(Graphics2D g2d){
