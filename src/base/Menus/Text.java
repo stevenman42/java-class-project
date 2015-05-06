0a65a209d1b4b2c3f6ea2fc6fbf099c67b7654ec
wwolfe <ChadWolfie@gmail.com>
2015-05-05 21:09:12 -0400

    where is god

---
+++ src/base/Menus/Text.java
@@ -0,0 +1,29 @@
+package base.Menus;
+
+import java.awt.Graphics2D;
+
+public class Text extends AbstractGuiComponent{
+	
+	private String text;
+	
+	public Text(String text){
+		
+	}
+	
+	public void render(Graphics2D g2d) {
+		
+	}
+
+	public void tick() {
+		
+	}
+
+	public boolean isMouseInBounds(int mx, int my) {
+		return false;
+	}
+	
+	public String getText(){
+		return text;
+	}
+	
+}
