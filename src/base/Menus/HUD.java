3cb941bf489db220c39a1be4057ddd3191520c93
wwolfe <ChadWolfie@gmail.com>
2015-05-08 13:50:08 -0400

    asdf

--- src/base/Menus/HUD.java
+++ src/base/Menus/HUD.java
@@ -4,6 +4,7 @@ import java.awt.Graphics2D;
 import java.util.ArrayList;
 
 import base.Game;
+import base.Entities.Player;
 import base.Items.Container;
 
 public class HUD {
@@ -12,13 +13,15 @@ public class HUD {
 	
 	private boolean showDebugData = true;
 	
-	private ArrayList<Container> containers;
+	private Container toRender;
 	private boolean renderContainers;
+	private boolean renderInvintory;
 	
 	public HUD(Game g){
 		game = g;
-		containers = new ArrayList<Container>();
+		toRender = Player.container;
 		renderContainers = false;
+		renderInvintory = false;
 	}
 	
 	public void render(Graphics2D g2d){
@@ -29,24 +32,37 @@ public class HUD {
 		}
 		
 		if(renderContainers){
-			renderContainers();
+			renderContainers(g2d);
+		}else if(renderInvintory){
+			renderInv(g2d);
 		}
 		
 	}
 	
-	public void addContainerToRender(Container c){
+	public void renderInvintory(){
+		renderInvintory = true;
+	}
+	
+	public void closeInvintory(){
+		renderInvintory = false;
+	}
+	
+	public void renderContainer(Container c){
 		renderContainers = true;
-		containers.add(c);
+		toRender = c;
 	}
 	
-	public void stopRenderingContainers(){
+	public void stopRenderingContainer(){
 		renderContainers = false;
-		containers.clear();
 	}
 	
-	public void renderContainers(){
+	private void renderContainers(Graphics2D g2d){
 		
+	}
+	
+	private void renderInv(Graphics2D g2d){
 		
+		g2d.drawImage(null, 0, 0, Game.WIDTH, Game.HEIGHT, null);
 		
 	}
 	
