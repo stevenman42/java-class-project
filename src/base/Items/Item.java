38ee600028bbd4940b0108c05baad582a0c6a070
wwolfe <ChadWolfie@gmail.com>
2015-05-08 13:04:54 -0400

    a better item structure

--- src/base/Items/Item.java
+++ src/base/Items/Item.java
@@ -1,15 +1,26 @@
 package base.Items;
 
+import java.awt.Image;
+import java.awt.Toolkit;
+
 public class Item {
 	
 	private String name;
 	
 	private String id;
 	
-	public Item(String name, String id){
+	private Image image;
+	
+	public Item(String name, String id, String img){
 		
 		this.name = name;
 		this.id = id;
+		
+		image = Toolkit.getDefaultToolkit().createImage(img);
+		
+		if(image == null){
+			System.err.println("coulden't create an image");
+		}
 		
 	}
 	
--- src/base/Items/ItemList.java
+++ src/base/Items/ItemList.java
@@ -2,6 +2,6 @@ package base.Items;
 
 public class ItemList {
 	
-	
+	public static final Item swastika = new Item("SWASTIKA", "0001", "RES/Textures/swastika.jpg");
 	
 }
